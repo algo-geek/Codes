44de16a45d13e835fff871f996eb7569e59028fe
algogeek <apara20_ug@ece.nits.ac.in>
2022-01-21 20:39:02 +0530

    eleven

---
+++ Hashing/longest subarray with sum 0.cpp
@@ -0,0 +1,13 @@
+int solve(int[] a){
+	int  max = 0;
+	for(int i = 0; i < a.length; ++i){
+		int sum = 0;
+		for(int j = i; j < a.length; ++j){
+			sum += a[j];
+			if(sum == 0){
+				max = max(max, j-i+1);
+			}
+		}
+	}
+	return max;
+   }
