6960d99ee2aecb62f640562d3788d9a830c1f80c
Apara Biswas <85570705+algo-geek@users.noreply.github.com>
2023-08-09 19:55:30 +0530

    Update check if subsequence.cpp

--- Strings/check if subsequence.cpp
+++ Strings/check if subsequence.cpp
@@ -34,17 +34,17 @@ int main() {
 using namespace std;
 
 bool isSubSeq(string s1, string s2, int n, int m){
-    if ( m == 0 )
+    if ( n == 0 )
         return true;
         
-    if ( n == 0 )
+    if ( m == 0 )
         return false;
     
     if ( s1[n-1] == s2[m-1] )
         return isSubSeq(s1, s2, n-1, m-1);
     
     else
-        return isSubSeq(s1, s2, n-1, m);
+        return isSubSeq(s1, s2, n, m-1);
 }
 
 int main() {
