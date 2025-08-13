406ab52ccccf613660b66069e7cf1000be85de63
Apara Biswas <85570705+algo-geek@users.noreply.github.com>
2025-08-13 15:29:33 +0530

    Update largest rectangle in histogram.cpp

--- Stacks and Queues/largest rectangle in histogram.cpp
+++ Stacks and Queues/largest rectangle in histogram.cpp
@@ -81,29 +81,28 @@ int main()
 #include <bits/stdc++.h>
 using namespace std;
 
-int getMaxArea(int arr[],int n){
-    stack <int> s;
-    int res=0;
-    int tp;
-    int curr;
-    
-    for(int i=0;i<n;i++){
-        while(s.empty()==false && arr[s.top()]>=arr[i]){
-            tp=s.top();s.pop();
-            curr=arr[tp]* (s.empty() ? i : i - s.top() - 1);
-            res=max(res,curr);
+int getMaxArea(vector<int>& heights) {
+        // insert in stack until a next smaller element is found
+        // pop elements such that
+        // max(nxtSmaller*(next_small_idx-poppedElement+1), poppedElement*(nxtSmallerIdx-poppedElement))
+        // pop out elements from stack->poppedElement*(lastPoppedElementIdx-poppedElementIdx+1)
+
+        stack<int>s;
+        heights.push_back(0);
+        int n=heights.size();
+        int ans=0;
+        
+        for(int i=0;i<n;i++){
+        while(!s.empty() && heights[i]<heights[s.top()]){
+            int top=heights[s.top()];
+            s.pop();
+            int w=s.empty()?-1:s.top();
+            ans=max(ans,top*(i-w-1));
         }
         s.push(i);
+        }
+        return ans;
     }
-    while(s.empty()==false){
-        tp=s.top();s.pop();
-        curr=arr[tp]* (s.empty() ? n : n - s.top() - 1);
-        res=max(res,curr);
-    }
-    
-    return res;
-    
-}
 
 int main() 
 { 
