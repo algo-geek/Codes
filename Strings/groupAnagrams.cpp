2b75b4097c1fa68bddf35df3c7d8bf670713cd1c
Apara Biswas <85570705+algo-geek@users.noreply.github.com>
2025-08-15 17:03:27 +0530

    Create groupAnagrams.cpp

---
+++ Strings/groupAnagrams.cpp
@@ -0,0 +1,16 @@
+// take each string, store it in a string t, sort t
+// push the original string as value of sorted string t as key
+// O(n) time & space
+vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string, vector<string>> mp;
+        for (string s : strs) {
+            string t = s; 
+            sort(t.begin(), t.end());
+            mp[t].push_back(s);
+        }
+        vector<vector<string>> anagrams;
+        for (auto p : mp) { 
+            anagrams.push_back(p.second);
+        }
+        return anagrams;
+}
