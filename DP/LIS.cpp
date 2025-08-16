a102e4c01f06bf6bdc996170d601b706085b373e
Apara Biswas <85570705+algo-geek@users.noreply.github.com>
2025-08-16 17:54:19 +0530

    Update LIS.cpp

--- DP/LIS.cpp
+++ DP/LIS.cpp
@@ -6,28 +6,18 @@ using namespace std;
 
 int LIS( int arr[], int n )  
 {  
-    int lis[n]; 
-   
-    lis[0] = 1;    
-  
-  
+    vector<int>lis(n,1); 
+	int res = 1;
     for (int i = 1; i < n; i++ )  
     { 
-        lis[i] = 1; 
         for (int j = 0; j < i; j++ )   
-            if ( arr[i] > arr[j])  
-                lis[i] = max(lis[i], lis[j] + 1);  
-    } 
-    
-    int res = lis[0];
-    
-    for(int i=0;i<n;i++)
-    {
-        res = max(lis[i], res);
-    }
-    
+		{
+				if ( arr[i] > arr[j])  
+                	lis[i] = max(lis[i], lis[j] + 1); 
+		}
+		res = max(lis[i], res);
+    }     
     return res;
- 
 }  
 
 int main() {
