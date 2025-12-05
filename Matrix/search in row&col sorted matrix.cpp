bc5a16f253c25c99aa1767ecdb15d11895d7d729
Apara Biswas <85570705+algo-geek@users.noreply.github.com>
2025-12-06 01:16:06 +0530

    Update search in row&col sorted matrix.cpp

--- Matrix/search in row&col sorted matrix.cpp
+++ Matrix/search in row&col sorted matrix.cpp
@@ -1,7 +1,8 @@
-
 // time: (R+C)
-#include <bits/stdc++.h> 
-using namespace std;
+//Idea is to remove a row or column in each comparison until an element is found. 
+//Start searching from the top-right corner of the matrix. 
+//x > curr ele: all ele in curr row are smaller than given number, entire row gets eliminated
+//x < curr ele: all ele in curr row are greater than given number, entire column gets eliminated
 
 const int R = 4, C = 4;
 
@@ -9,8 +10,7 @@ void search(int mat[R][C], int x)
 {
 	int i  = 0, j = C - 1;
  
- // top left
- // bottom right can also be used
+ // top right
 	while(i < R && j >= 0)
 	{
 		if(mat[i][j] == x)
