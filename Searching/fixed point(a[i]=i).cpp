f423e074ec586db36b162018537d7890b8aa8743
Apara Biswas <85570705+algo-geek@users.noreply.github.com>
2025-12-05 17:28:03 +0530

    Update fixed point(a[i]=i).cpp

--- Searching/fixed point(a[i]=i).cpp
+++ Searching/fixed point(a[i]=i).cpp
@@ -3,31 +3,26 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int binarySearch(int arr[], int low, int high)
-{
-	if (high >= low) {
-		int mid = low + (high - low) / 2;
-		if (mid == arr[mid])
-			return mid;
-		int res = -1;
-		if (mid + 1 <= arr[high])
-			res = binarySearch(arr, (mid + 1), high);
-		if (res != -1)
-			return res;
-		if (mid - 1 >= arr[low])
-			return binarySearch(arr, low, (mid - 1));
-	}
+int binarySearch(vector<int> &arr) {
 
-	return -1;
+    int low = 0, high = arr.size() - 1;
+
+    while (low <= high) {
+        int mid = low + (high - low) / 2;
+
+        if (arr[mid] == mid)
+            return mid;
+        else if (arr[mid] < mid)
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+
+    // If no fixed point is found
+    return -1;
 }
 
-int main()
-{
-	int arr[10] = { -10, -1, 0, 3, 10, 11, 30, 50, 100 };
-	int n = sizeof(arr) / sizeof(arr[0]);
-	cout << "Fixed Point is " << binarySearch(arr, 0, n - 1);
-	return 0;
-}
+
 
 // duplicates allowed
 #include <bits/stdc++.h>
@@ -52,19 +47,6 @@ int magicIndex(int* arr, int start, int end)
 		return left;
 
 	return magicIndex(arr, max(midValue, midIndex + 1), end);
-}
-
-int main()
-{
-	int arr[] = { -10, -5, 2, 2, 2, 3, 4, 7,
-								9, 12, 13 };
-	int n = sizeof(arr) / sizeof(arr[0]);
-	int index = magicIndex(arr, 0, n - 1);
-	if (index == -1)
-		cout << "No Magic Index";
-	else
-		cout << "Magic Index is : " << index;
-	return 0;
 }
 
 
