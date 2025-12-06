3f7642ecb4682599c46d43a029379d5b0dbb350d
Apara Biswas <85570705+algo-geek@users.noreply.github.com>
2025-12-07 00:34:34 +0530

    Update trapping rainwater.cpp

--- Array/trapping rainwater.cpp
+++ Array/trapping rainwater.cpp
@@ -1,53 +1,29 @@
 //naive
-#include <iostream>
-#include <cmath>
-using namespace std;
-
-
 int getWater(int arr[], int n)
 {
 	int res = 0;
-
 	for(int i = 1; i < n - 1; i++)
 	{
 		int lMax = arr[i];
-
 		for(int j = 0; j < i; j++)
 			lMax = max(lMax, arr[j]);
 
 		int rMax = arr[i];
-
 		for(int j = i + 1; j < n; j++)
 			rMax = max(rMax, arr[j]);
 
 		res = res + (min(lMax, rMax) - arr[i]);
 	}
-
 	return res;
 }
 
-
-int main() {
-	
-     int arr[] = {3, 0, 1, 2, 5}, n = 5;
-
-     cout<<getWater(arr, n);
-    
-}
-
 //efficient //space: O(n)
-#include <iostream>
-#include <cmath>
-using namespace std;
-
-
 int getWater(int arr[], int n)
 {
     int left[n];
     int right[n];
  
     int water = 0;
- 
     // Fill left array
     left[0] = arr[0];
     for (int i = 1; i < n; i++)
@@ -67,60 +43,32 @@ int getWater(int arr[], int n)
         water += var - arr[i];
       }
     }
-        
- 
     return water;
 }
 
-
-int main() {
-	
-     int arr[] = {3, 0, 1, 2, 5}, n = 5;
-
-     cout<<getWater(arr, n);
-    
-}
-
 // O(1) space
-
-#include <iostream>
-using namespace std;
-
-int findWater(int arr[], int n)
-{
-	int result = 0;
-	int left_max = 0, right_max = 0;
-	int lo = 0, hi = n - 1;
-
-	while (lo <= hi) 
-	{
-		if (arr[lo] < arr[hi]) 
-		{
-			if (arr[lo] > left_max)
-				left_max = arr[lo];
-			else
-				result += left_max - arr[lo];
-
-			lo++;
-		}
-		else 
-		{
-			if (arr[hi] > right_max)
-				right_max = arr[hi];
-			else
-				result += right_max - arr[hi];
-
-			hi--;
-		}
-	}
-
-	return result;
-}
-
-int main()
-{
-	int arr[] = { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
-	int n = sizeof(arr) / sizeof(arr[0]);
-	cout << "Maximum water that can be accumulated is "
-		<< findWater(arr, n);
-}
+	int trap(vector<int>& height) {
+        int n=height.size(), i=1, j=n-2, prefixmax=height[0], suffixmax=height[n-1];
+        int ans=0;
+        while(i<=j){
+            if(prefixmax<=suffixmax){
+                if(height[i]<=prefixmax){
+                    ans+=prefixmax-height[i];
+                }
+                else{
+                    prefixmax=height[i];
+                }
+                i++;
+            }
+            else{
+                if(height[j]<suffixmax){
+                    ans+=suffixmax-height[j];
+                }
+                else{
+                    suffixmax=height[j];
+                }
+                j--;
+            }
+        }
+        return ans;
+    }
